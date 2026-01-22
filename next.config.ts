a3c293385de8aae02007b2b4e4453311443b0243
Michal Pechardo <michalpechardo@gmail.com>
2026-01-22 12:40:54 -0800

    Simplify config and downgrade to Next.js 15.2

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // Increase body size limit for uploads (though we're using signed URLs)
-  serverRuntimeConfig: {
-    maxDuration: 60,
-  },
   // Headers required for FFmpeg.wasm (SharedArrayBuffer support)
   async headers() {
     return [
@@ -23,20 +19,6 @@ const nextConfig: NextConfig = {
         ],
       },
     ];
-  },
-  // Webpack configuration to handle optional local modules
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Ignore missing optional modules
-      config.plugins = config.plugins || [];
-      config.plugins.push(
-        new (require('webpack').IgnorePlugin)({
-          resourceRegExp: /^\.\/local-(storage|queue)$/,
-          contextRegExp: /src\/lib$/,
-        })
-      );
-    }
-    return config;
   },
 };
 
